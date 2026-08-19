9a77ae999d0272cdba66f345ff96d40787463bb2
Treowian <odilon93@gmail.com>
2026-08-19 15:24:48 +0200

    Init

---
+++ sw.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch', function(event) {
+    // Un cache basique ou simplement une interception vide pour tromper Android 
+    // et permettre l'installation sur l'écran d'accueil.
+});
